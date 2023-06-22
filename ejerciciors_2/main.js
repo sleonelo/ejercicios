506746d24609815ab94d39f2054c0bf4fa47723f
silvio olmos <olmoscarrizosilvioleonel@gmail.com>
2023-06-22 09:13:47 -0300

    commit 2

--- ejerciciors_2/main.js
+++ ejerciciors_2/main.js
@@ -1,76 +1,127 @@
-function promedio() {
+function obtenerDatos() {
   let nombre = document.getElementById("nombre").value;
   let apellido = document.getElementById("apellido").value;
   let dni = document.getElementById("dni").value;
   let materia = document.getElementById("materia").value;
   let notas = document.getElementsByClassName("notas");
   let valores = [];
-    for (let i = 0; i < notas.length; i++) {
-      valores.push(notas[i].value);
-    }
-    if (nombre == "" || apellido == "" || dni == "" || materia == "") {
-      alert("debe completar todos los campos!");
+
+  for (let i = 0; i < notas.length; i++) {
+    valores.push(notas[i].value);
+  }
+
+  if (nombre == "" || apellido == "" || dni == "" || materia == "") {
+    alert("Debe completar todos los campos!");
+    return false;
+  }
+
+  for (let i = 0; i < valores.length; i++) {
+    if (valores[i] == "") {
+      alert("Debe completar todos los campos de notas");
       return false;
     }
-    for (let i = 0; i < valores.length; i++) {
-      if (valores[i]==""){
-        alert("debe completar todos los campos de notas");
-        return false;
-      };
-    }
+  }
+
+  return {
+    nombre: nombre,
+    apellido: apellido,
+    dni: dni,
+    materia: materia,
+    valores: valores,
+  };
+}
+
+function promedio() {
+  let datos = obtenerDatos();
+
+  if (datos) {
+    let parametros = {
+      RS_MetodoAEjecutar: "funcion_servidor",
+      RS_parametros: [
+        datos.nombre,
+        datos.apellido,
+        datos.dni,
+        datos.materia,
+        datos.valores,
+      ],
+    };
+  // let nombre = document.getElementById("nombre").value;
+  // let apellido = document.getElementById("apellido").value;
+  // let dni = document.getElementById("dni").value;
+  // let materia = document.getElementById("materia").value;
+  // let notas = document.getElementsByClassName("notas");
+  // let valores = [];
+  //   for (let i = 0; i < notas.length; i++) {
+  //     valores.push(notas[i].value);
+  //   }
+  //   if (nombre == "" || apellido == "" || dni == "" || materia == "") {
+  //     alert("debe completar todos los campos!");
+  //     return false;
+  //   }
+  //   for (let i = 0; i < valores.length; i++) {
+  //     if (valores[i]==""){
+  //       alert("debe completar todos los campos de notas");
+  //       return false;
+  //     };
+  //   }
   
 
-  let parametros = {
-    RS_MetodoAEjecutar: 'funcion_servidor',
-    RS_parametros: [nombre, apellido, dni, materia,valores],
-  };
-  $.ajax({
-    type: "POST",
-    dataType: "html",
-    url: 'servidor.php',
-    data: parametros,
-    success: function (cadena) {
-      datos = JSON.parse(cadena);
-      if (datos.mensaje) {
-        // la devolucion es un mensaje, ya sea de error o de que todo estuvo bien
-        $("#resultado").html(datos.mensaje);
-      } else {
-        // se devuelve un arreglo de datos
-        for (i = 0; i < datos.length; i++) {
-          $("#resultado").html(datos.mensaje);
-        }
-      }
-      // alert(cadena.split(',') + " ///// " + JSON.stringify(cadena));
-    },
+  // let parametros = {
+  //   RS_MetodoAEjecutar: 'funcion_servidor',
+  //   RS_parametros: [nombre, apellido, dni, materia,valores],
+  // };
+  llamarAjax(parametros, function (respuesta) {
+    $("#resultado").html(respuesta);
   });
+};
 }
 
 function mayor() {
-  let nombre = document.getElementById("nombre").value;
-  let apellido = document.getElementById("apellido").value;
-  let dni = document.getElementById("dni").value;
-  let materia = document.getElementById("materia").value;
-  let notas = document.getElementsByClassName("notas");
-  let valores = [];
-    for (let i = 0; i < notas.length; i++) {
-      valores.push(notas[i].value);
-    }
-    if (nombre == "" || apellido == "" || dni == "" || materia == "") {
-      alert("debe completar todos los campos!");
-      return false;
-    }
-    for (let i = 0; i < valores.length; i++) {
-      if (valores[i]==""){
-        alert("debe completar todos los campos de notas");
-        return false;
-      };
-    }
+  let datos = obtenerDatos();
+
+  if (datos) {
+    let parametros = {
+      RS_MetodoAEjecutar: "funcion_servidor1",
+      RS_parametros: [
+        datos.nombre,
+        datos.apellido,
+        datos.dni,
+        datos.materia,
+        datos.valores,
+      ],
+    };
+  // let nombre = document.getElementById("nombre").value;
+  // let apellido = document.getElementById("apellido").value;
+  // let dni = document.getElementById("dni").value;
+  // let materia = document.getElementById("materia").value;
+  // let notas = document.getElementsByClassName("notas");
+  // let valores = [];
+  //   for (let i = 0; i < notas.length; i++) {
+  //     valores.push(notas[i].value);
+  //   }
+  //   if (nombre == "" || apellido == "" || dni == "" || materia == "") {
+  //     alert("debe completar todos los campos!");
+  //     return false;
+  //   }
+  //   for (let i = 0; i < valores.length; i++) {
+  //     if (valores[i]==""){
+  //       alert("debe completar todos los campos de notas");
+  //       return false;
+  //     };
+  //   }
   
 
-  let parametros = {
-    RS_MetodoAEjecutar: 'funcion_servidor1',
-    RS_parametros: [nombre, apellido, dni, materia,valores],
-  };
+  // let parametros = {
+  //   RS_MetodoAEjecutar: 'funcion_servidor1',
+  //   RS_parametros: [nombre, apellido, dni, materia,valores],
+  // };
+  llamarAjax(parametros, function (respuesta) {
+    $("#resultado1").html(respuesta);
+  });
+};
+}
+
+function llamarAjax(parametros, callback) {
   $.ajax({
     type: "POST",
     dataType: "html",
@@ -80,14 +131,13 @@ function mayor() {
       datos = JSON.parse(cadena);
       if (datos.mensaje) {
         // la devolucion es un mensaje, ya sea de error o de que todo estuvo bien
-        $("#resultado1").html(datos.mensaje);
+        callback(datos.mensaje);
       } else {
         // se devuelve un arreglo de datos
         for (i = 0; i < datos.length; i++) {
-          $("#resultado1").html(datos.mensaje);
+          callback(datos[i]);
         }
       }
-      // alert(cadena.split(',') + " ///// " + JSON.stringify(cadena));
     },
   });
 }
